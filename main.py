22d0db9a6f5dd25c448949d61eca0dd5fb68e4c6
Hadi <haditrumboo@gmail.com>
2026-06-19 22:28:32 +0530

    feat: add human approval middleware for tool execution

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain.agents import create_agent
+from langchain.agents.middleware import wrap_tool_call
+from langchain_core.messages import ToolMessage
 from langchain_core.messages import HumanMessage
 from pypdf import PdfReader
 from pathlib import Path
 from langchain.tools import tool
+from fpdf import FPDF
 from rich import print
 from langchain_core.messages import HumanMessage, AIMessage
 import os
@@ -15,6 +18,7 @@ llm = ChatGroq(
     model="llama-3.3-70b-versatile",
     api_key=os.getenv("GROQ_API_KEY")
 )
+
 
 @tool
 def extract_pdf(pdf_name: str) -> str:
@@ -44,12 +48,76 @@ def extract_pdf(pdf_name: str) -> str:
 
     return f"No PDF matching '{pdf_name}' found. Available PDFs: {available}"
 
+
+@tool
+def save_as_txt(filename: str, content: str) -> str:
+    """
+    Save content to a text file.
+    """
+
+    output_dir = Path("outputs")
+    output_dir.mkdir(exist_ok=True)
+
+    filepath = output_dir / f"{filename}.txt"
+
+    filepath.write_text(content, encoding="utf-8")
+
+    return f"Saved text file: {filepath}"
+
+@tool
+def save_as_pdf(filename: str, content: str) -> str:
+    """
+    Save content to a PDF file.
+    """
+
+    output_dir = Path("outputs")
+    output_dir.mkdir(exist_ok=True)
+
+    filepath = output_dir / f"{filename}.pdf"
+
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Arial", size=12)
+
+    pdf.multi_cell(0, 10, content)
+
+    pdf.output(str(filepath))
+
+    return f"Saved PDF file: {filepath}"
+
+# toolcall
+
+@wrap_tool_call
+def human_approval(request, handler):
+
+    tool_name = request.tool_call["name"]
+
+    if tool_name in [
+        "save_as_txt",
+        "save_as_pdf"
+    ]:
+        confirm = input(
+            f"Agent wants to create a file using '{tool_name}'. Approve? (y/n): "
+        )
+
+        if confirm.lower() not in ["y", "yes"]:
+            return ToolMessage(
+                content="Tool call denied by user.",
+                tool_call_id=request.tool_call["id"]
+            )
+
+    return handler(request)
+
 print("MODEL:", llm.model_name)
 
 # create_agent
 agent = create_agent(
     model=llm,
-    tools=[extract_pdf],
+    tools=[
+        extract_pdf,
+        save_as_txt,
+        save_as_pdf],
+    middleware=[human_approval],
     system_prompt="""
 You are a helpful student assistant.
 
@@ -83,10 +151,10 @@ while True:
         })
 
 
-        last_message = result["messages"][-1]
         
+        messages = result["messages"]
 
-        print("Bot:", last_message.content)
+        print("Bot:", messages[-1].content) 
 
     except Exception as e:
         print("ERROR:", repr(e))
